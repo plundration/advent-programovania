0aa06edb79342e501fe5b05e5082ca3b895172e9
plundration <matouinhell@gmail.com>
2022-12-07 16:35:37 +0100

    lmao

--- a.ts
+++
@@ -1,6 +0,0 @@
-import Pocketbase from 'pocketbase';
-const adminPb = new Pocketbase('https://advent.gympos.sk/pb');
-
-adminPb.admins.authWithPassword('sveltekitadmin@gmail.com', 'vmTPLGRD3seBfdP')
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
